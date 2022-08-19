2430fa56a72d15d0ea076e22f6a5450ca3b03f03
Johnny Lim <izeye@naver.com>
2022-08-19 21:08:53 +0900

    Initial commit

---
+++ src/test/java/learningtest/java/util/zip/CRC32Tests.java
@@ -0,0 +1,31 @@
+package learningtest.java.util.zip;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.zip.CRC32;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link java.util.zip.CRC32}.
+ *
+ * @author Johnny Lim
+ */
+class CRC32Tests {
+
+    @Test
+    void test() {
+        CRC32 crc32 = new CRC32();
+        crc32.update("Hello".getBytes());
+        crc32.update("World".getBytes());
+        long value = crc32.getValue();
+        assertThat(value).isNotZero();
+
+        crc32.reset();
+        assertThat(crc32.getValue()).isZero();
+
+        crc32.update("HelloWorld".getBytes());
+        assertThat(crc32.getValue()).isEqualTo(value);
+    }
+
+}
